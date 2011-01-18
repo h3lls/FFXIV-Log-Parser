48ef173d24d50313ff67d750b8f337a1e6bb426b
H3lls <savagesoul@gmail.com>
2011-01-17 22:15:40 -0800

    Initial Commit.

---
+++ setup.py
@@ -0,0 +1,13 @@
+import sys
+
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+        name = "FFXIVLogParser",
+        version = "1.4",
+        description = "FFXIV Log Parser",
+        executables = [Executable("logparse.py", base = base)])
